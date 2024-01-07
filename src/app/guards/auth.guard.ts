419b503b2c8d1df7da5160d209d7da542eee65ab
Martin Nzuki <iamnzuki@gmail.com>
2024-01-07 22:50:53 +0300

    Auth guard logic

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,10 @@
 import { CanActivateFn } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const usr = localStorage.getItem('auth');
+  if (!usr) return false;
+  // const user = JSON.parse(usr);
+  // if (!user?.token) return false;
+  // return true;
   return true;
 };
