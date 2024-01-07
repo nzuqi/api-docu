312c01112942027d4134f4e037d695c32bc845d8
Martin Nzuki <iamnzuki@gmail.com>
2024-01-07 15:20:36 +0300

    Sidenav for small screens enhancements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatIconModule
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { SidenavService } from 'src/app/services/sidenav.service';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent {
+  showMenu: boolean = false;
+
+  constructor(public sidenavService: SidenavService) { }
+
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
+    const sidenav = document.querySelector('app-sidenav');
+    this.showMenu ? sidenav?.classList.add('show-nav-small-device') : sidenav?.classList.remove('show-nav-small-device');
+  }
+}
